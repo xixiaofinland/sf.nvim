2e0bb814464abae240257570e7cb3b5f82cf969c
xi xiao <tdxiaoxi2@gmail.com>
2024-01-05 18:36:56 +0200

    +

--- lua/sf/term/init.lua
+++ lua/sf/term/init.lua
@@ -1,6 +1,7 @@
 local Term = require "sf.term.terminal"
 local t = Term:new()
 local U = require "sf.term.util"
+local TS = require('sf.ts')
 
 local M = {}
 
@@ -21,6 +22,16 @@ end
 
 function M.run(c)
   local cmd = U.expand_cmd(c)
+  t:run(cmd)
+end
+
+function M.runAllTestsInCurrentFile()
+  local test_class_name = TS.get_test_class_name()
+  if test_class_name == nil then
+    return vim.notify('Not in a test class', vim.log.levels.ERROR)
+  end
+
+  local cmd = "sf apex run test --class-names " .. test_class_name .. " --result-format human" .. U.get_target_org()
   t:run(cmd)
 end
 
--- lua/sf/ts/init.lua
+++ lua/sf/ts/init.lua
@@ -2,7 +2,7 @@ local ts = vim.treesitter
 local parsers = require('nvim-treesitter.parsers')
 local parser = parsers.get_parser()
 local lang = parser:lang()
-local qs = [[
+local test_annotation = [[
     (method_declaration
       (modifiers
         (annotation
@@ -10,7 +10,24 @@ local qs = [[
       name: (identifier) @meth_name
     )
   ]]
-local apex_test_meth_query = ts.query.parse(lang, qs)
+local apex_test_meth_query = ts.query.parse(lang, test_annotation)
+
+local class_name = [[
+    (class_declaration
+      name: (identifier) @class_name
+    )
+  ]]
+local class_name_query = ts.query.parse(lang, class_name)
+
+local test_class_name = [[
+    (class_declaration
+      (modifiers
+        (annotation
+          (identifier) @anno (#match? @anno "^[iI][sS][tT][eE][sS][tT]$" )))
+      name: (identifier) @class_name
+    )
+  ]]
+local test_class_name_query = ts.query.parse(lang, test_class_name)
 
 local tree = parser:parse()[1]
 local root = tree:root()
@@ -18,15 +35,31 @@ local root = tree:root()
 local M = {}
 local H = {}
 
+M.get_class_name = function()
+  local result = H.get_matched_node_names(class_name_query, 1, root)
+  if not next(result) then
+    return nil
+  end
+  return result[1]
+end
+
+M.get_test_class_name = function()
+  local result = H.get_matched_node_names(test_class_name_query, 2, root)
+  if not next(result) then
+    return nil
+  end
+  return result[1]
+end
+
 M.get_test_method_names_in_curr_file = function()
-  return H.get_matched_node_names(apex_test_meth_query, root)
+  return H.get_matched_node_names(apex_test_meth_query, 2, root)
 end
 
 M.get_curr_method_name = function()
   local curr_node = ts.get_node()
   while curr_node ~= nil do
     if curr_node:type() == 'method_declaration' then
-      local names = H.get_matched_node_names(apex_test_meth_query, curr_node)
+      local names = H.get_matched_node_names(apex_test_meth_query, 2, curr_node)
       if names ~= nil then
         return names[1]
       end
@@ -38,10 +71,10 @@ end
 
 --- ================== Help ========================
 
-H.get_matched_node_names = function(query, node)
+H.get_matched_node_names = function(query, anno_index, node)
   local names = {}
   for _, matches, _ in query:iter_matches(node, 0) do
-    local name = ts.get_node_text(matches[2], 0)
+    local name = ts.get_node_text(matches[anno_index], 0)
     table.insert(names, name)
   end
   return names
