cb48d34d1daa92bc6e146f2e952dcd31c5734fc4
xi xiao <tdxiaoxi2@gmail.com>
2024-01-05 18:52:56 +0200

    +

--- lua/sf/term/init.lua
+++ lua/sf/term/init.lua
@@ -20,13 +20,28 @@ function M.retrieve()
   t:run(cmd)
 end
 
+function M.runCurrentTest()
+  local test_class_name = TS.get_test_class_name()
+  if test_class_name == nil then
+    return vim.notify('Not in a test class', vim.log.levels.ERROR)
+  end
+
+  local test_name = TS.get_curr_method_name()
+  if test_name == nil then
+    return vim.notify('Not in a test method', vim.log.levels.ERROR)
+  end
+
+  local cmd = "sf apex run test --tests " .. test_class_name .. "." .. test_name .. " --result-format human " .. U.get_target_org()
+  t:run(cmd)
+end
+
 function M.runAllTestsInCurrentFile()
   local test_class_name = TS.get_test_class_name()
   if test_class_name == nil then
     return vim.notify('Not in a test class', vim.log.levels.ERROR)
   end
 
-  local cmd = "sf apex run test --class-names " .. test_class_name .. " --result-format human" .. U.get_target_org()
+  local cmd = "sf apex run test --class-names " .. test_class_name .. " --result-format human " .. U.get_target_org()
   t:run(cmd)
 end
 
