4037fcebe72a88f994b5c2de23c812f3b5845639
xi xiao <tdxiaoxi2@gmail.com>
2024-02-04 16:18:45 +0200

    +

--- plugin/sf.lua
+++ plugin/sf.lua
@@ -8,14 +8,3 @@ vim.filetype.add({
     sosl = 'sosl',
   }
 })
-
-local augroup = vim.api.nvim_create_augroup("Sf", { clear = true })
-
-vim.api.nvim_create_autocmd({ 'VimEnter' }, {
-  group = augroup,
-  desc = "Run sf org cmd and store org info in the plugin",
-  once = true,
-  callback = function()
-    require('sf.org').fetch_org_list()
-  end,
-})
---
+++ tests/test_sf.lua
@@ -0,0 +1,24 @@
+local new_set = MiniTest.new_set
+local expect, eq = MiniTest.expect, MiniTest.expect.equality
+
+local child = MiniTest.new_child_neovim()
+
+local T = new_set({
+  hooks = {
+    pre_case = function()
+      child.restart({ '-u', 'scripts/minimal_init.lua' })
+      child.lua([[M = require('sf')]])
+    end,
+
+    post_once = child.stop,
+  },
+})
+
+T['error'] = function()
+  -- it should throw as `target_org` defaults to empty
+  expect.error(function()
+    M.get()
+  end)
+end
+
+return T
