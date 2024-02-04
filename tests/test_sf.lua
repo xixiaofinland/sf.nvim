35ef4f85921671981a7b94fea91b4be68fed1a34
xi xiao <tdxiaoxi2@gmail.com>
2024-02-04 23:01:02 +0200

    basic test pass (no child process)

--- tests/test_sf.lua
+++ tests/test_sf.lua
@@ -1,23 +1,19 @@
 local new_set = MiniTest.new_set
 local expect, eq = MiniTest.expect, MiniTest.expect.equality
 
--- local child = MiniTest.new_child_neovim()
-
-local T = new_set()
-
--- local T = new_set({
---   hooks = {
---     pre_case = function()
---       child.restart({ '-u', 'scripts/minimal_init.lua' })
---       child.lua([[M = require('sf')]])
---     end,
---
---     post_once = child.stop,
---   },
--- })
+local T = new_set({
+  hooks = {
+    pre_case = function()
+      M = require('sf')
+      T = require('sf.term')
+      A = require('telescope')
+      U = require('sf.ts')
+    end,
+    -- post_once = child.stop,
+  },
+})
 
 T['error'] = function()
-  -- it should throw as `target_org` defaults to empty
   expect.error(function()
     M.get()
   end)
