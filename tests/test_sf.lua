f7e969fe4be34f773b744e430eb7f1dfcc2fd9ac
xi xiao <tdxiaoxi2@gmail.com>
2024-02-04 17:38:15 +0200

    +

--- tests/test_sf.lua
+++ tests/test_sf.lua
@@ -1,18 +1,20 @@
 local new_set = MiniTest.new_set
 local expect, eq = MiniTest.expect, MiniTest.expect.equality
 
-local child = MiniTest.new_child_neovim()
+-- local child = MiniTest.new_child_neovim()
 
-local T = new_set({
-  hooks = {
-    pre_case = function()
-      child.restart({ '-u', 'scripts/minimal_init.lua' })
-      child.lua([[M = require('sf')]])
-    end,
+local T = new_set()
 
-    post_once = child.stop,
-  },
-})
+-- local T = new_set({
+--   hooks = {
+--     pre_case = function()
+--       child.restart({ '-u', 'scripts/minimal_init.lua' })
+--       child.lua([[M = require('sf')]])
+--     end,
+--
+--     post_once = child.stop,
+--   },
+-- })
 
 T['error'] = function()
   -- it should throw as `target_org` defaults to empty
