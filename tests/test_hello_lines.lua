6bb3f2ce5e22df576afe8617faee65669daed514
xi xiao <tdxiaoxi2@gmail.com>
2024-02-04 14:35:31 +0200

    +

--- tests/test_hello_lines.lua
+++ tests/test_hello_lines.lua
@@ -31,29 +31,4 @@ T['compute()']['works'] = function()
   -- expected result
   eq(child.lua_get([[M.compute({'a', 'b'})]]), { 'Hello a', 'Hello b' })
 end
-
--- T['compute()']['uses correct defaults'] = function()
---   eq(child.lua_get([[M.compute()]]), { 'Hello world' })
--- end
---
--- -- Make parametrized tests. This will create three copies of each case
--- T['set_lines()'] = new_set({ parametrize = { {}, { 0, { 'a' } }, { 0, { 1, 2, 3 } } } })
---
--- -- Use arguments from test parametrization
--- T['set_lines()']['works'] = function(buf_id, lines)
---   -- Directly modify some options to make better test
---   child.o.lines, child.o.columns = 10, 20
---   child.bo.readonly = false
---
---   -- Execute Lua code without returning value
---   child.lua('M.set_lines(...)', { buf_id, lines })
---
---   -- Test screen state. On first run it will automatically create reference
---   -- screenshots with text and look information in predefined location. On
---   -- later runs it will compare current screenshot with reference. Will throw
---   -- informative error with helpful information if they don't match exactly.
---   expect.reference_screenshot(child.get_screenshot())
--- end
---
--- -- Return test set which will be collected and execute inside `MiniTest.run()`
 return T
