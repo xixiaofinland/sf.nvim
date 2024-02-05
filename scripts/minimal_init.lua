318465be8a00860f5ac9af87ab9dc0f63b040d6d
xi xiao <tdxiaoxi2@gmail.com>
2024-02-05 19:46:03 +0200

    plugin folder doesn't block anymore!

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -2,9 +2,9 @@ vim.cmd([[let &rtp.=','.getcwd()]])
 
 if #vim.api.nvim_list_uis() == 0 then
   vim.cmd('set rtp+=deps/mini.nvim')
-  vim.cmd('set rtp+=deps/plenary.nvim')
-  vim.cmd('set rtp+=deps/telescope.nvim')
-  vim.cmd('set rtp+=deps/nvim-treesitter')
-
   require('mini.test').setup()
 end
+
+vim.cmd('set rtp+=deps/plenary')
+vim.cmd('set rtp+=deps/telescope.nvim')
+vim.cmd('set rtp+=deps/nvim-treesitter')
--- tests/test_sf.lua
+++ tests/test_sf.lua
@@ -1,22 +1,20 @@
 local new_set = MiniTest.new_set
 local expect, eq = MiniTest.expect, MiniTest.expect.equality
+local child = MiniTest.new_child_neovim()
 
 local T = new_set({
   hooks = {
     pre_case = function()
-      M = require('sf')
-      T = require('sf.term')
-      A = require('telescope')
-      U = require('sf.ts')
+      child.restart({ '-u', 'scripts/minimal_init.lua' })
+      child.lua([[M = require('sf')]])
     end,
-    -- post_once = child.stop,
+    post_once = child.stop,
   },
 })
 
 T['error'] = function()
-  expect.error(function()
-    M.get()
-  end)
+  expect.error(child.lua([[M.get()]]))
+  -- eq(child.lua([[return M.compute({'a', 'b'})]]), { 'Hello a', 'Hello b' })
 end
 
 return T
