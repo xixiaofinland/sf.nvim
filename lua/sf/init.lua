a29419a633aa01616c440ff8439a1b6b78b89455
xi xiao <tdxiaoxi2@gmail.com>
2024-02-25 06:20:56 +0200

    update

--- lua/sf/init.lua
+++ lua/sf/init.lua
@@ -5,14 +5,10 @@ local Metadata = require('sf.md');
 local Test = require('sf.test');
 local Sf = {}
 
---- |Sf.repeat_last_tests| uses it to conveniently execute the last executed command.
---- Test run commands like |Sf.run_all_tests_in_this_file| will save value into this variable.
-Sf.last_tests = Util.last_tests
-
 --- Toggle the SFTerm float window.
 Sf.get_target_org = Util.get
 
--- Copy current file name without dot-after, e.g. copy "Hello" from "Hello.cls"
+-- A convenient way to quickly copy file name trimming dot-after, e.g. copy "Hello" from "Hello.cls"
 Sf.copy_apex_name = Util.copy_apex_name
 
 -- From Term module ==========================================================
@@ -20,10 +16,10 @@ Sf.copy_apex_name = Util.copy_apex_name
 --- Toggle the SFTerm float window.
 Sf.toggle_term = Term.toggle
 
---- Save the file in the current buffer and push to target_org. The command is sent to SFTerm.
+--- Save the file in the current buffer and push to target_org.
 Sf.save_and_push = Term.save_and_push
 
---- Retrieve the file in the current buffer from target_org. The command is sent to SFTerm.
+--- Retrieve the file in the current buffer from target_org.
 Sf.retrieve = Term.retrieve
 
 --- Terminate the running command in SFTerm.
@@ -37,23 +33,23 @@ Sf.run = Term.run
 
 -- From Term module ==========================================================
 
---- It runs "sf org list" command under the hood and stores the org list.
---- If a target_org is found, the value is saved into "target_org" variable.
+--- Run "sf org list" command under the hood and stores the org list.
+--- If a target_org is found, the value is saved into "require('sf.util').target_org", an internal variable.
 Sf.fetch_org_list = Org.fetch_org_list
 
---- It displays the list of orgs, and allows you to define the target_org.
---- It runs "sf config set target-org" command under the hood to set the target_org.
+--- Display the list of available orgs, and allow to define the target_org.
+--- Run "sf config set target-org" command under the hood to set the target_org.
 Sf.set_target_org = Org.set_target_org
 
---- sf command allows to define a global target_org.
---- It runs "sf config set target-org --global " command under the hood.
+--- Salesforce sf command allows users to define a global target_org.
+--- Run "sf config set target-org --global " command under the hood.
 Sf.set_global_target_org = Org.set_global_target_org
 
---- It fetches the file in the current buffer from target_org and display in the Nvim diff mode.
+--- Fetch the file in the current buffer from target_org and display in the Nvim diff mode.
 --- The left window displays the target_org verison, the right window displays the local verison.
 Sf.diff_in_target_org = Org.diff_in_target_org
 
---- Similar to |diff_in_target_org|, you can choose which org to diff with.
+--- Similar to |Sf.diff_in_target_org|, you can choose which org to diff with.
 --- The left window displays the org verison, the right window displays the local verison.
 Sf.diff_in_org = Org.diff_in_org
 
@@ -63,40 +59,42 @@ Sf.diff_in_org = Org.diff_in_org
 Sf.pull_md_json = Metadata.pull_md_json
 
 --- Choose a specific metadata file to retrieve.
---- Its popup list depends on data retrieved by |retrieve_metadata_lists| in prior.
+--- Its popup list depends on data retrieved by |Sf.retrieve_metadata_lists| in prior.
 Sf.list_md_to_retrieve = Metadata.list_md_to_retrieve
 
---- Pulls defined md files to local json file and list them in telescope for retrieving
---- it is `pull_md_json()` then `list_md_to_retrieve()` in one go.
+--- Pull pre-defined metadata files to local and list them in telescope for further retrieving
+--- It's |Sf.pull_md_json| and |Sf.list_md_to_retrieve| in one go.
 Sf.pull_and_list_md = Metadata.pull_and_list_md
 
 --- Download metadata-type list, e.g. ApexClass, LWC, Aura, FlexiPage, etc. as a Json file into the project root path "md" folder.
 Sf.pull_md_type_json = Metadata.pull_md_type_json
 
 --- Select a specific metadata-type to download all its files. For example, download all ApexClass files.
---- Its popup list depends on data retrieved by |pull_metadata_type_list| in prior.
+--- Its popup list depends on data retrieved by |Sf.pull_metadata_type_list| in prior.
 Sf.list_md_type_to_retrieve = Metadata.list_md_type_to_retrieve
 
---- Pulls metadata-types to a local json file and list them in telescope for retrieving all corresponding type files
---- `pull_md_type_json()` then `list_md_type_to_retrieve()` in one go.
+--- Pull the list of metadata-types into a local json file, and list them in a pop-up.
+--- It's |Sf.pull_md_type_json| and |Sf.list_md_type_to_retrieve| in one go.
 Sf.pull_and_list_md_type = Metadata.pull_and_list_md_type
 
---- Use the word under the cursor and attempt to retrieve as a Apex name from target_org.
+--- Uses the word under the cursor as Apex name to attempt to retreive from the org.
+--- A convenient way to quickly pull Apex into local.
 Sf.retrieve_apex_under_cursor = Metadata.retrieve_apex_under_cursor
 
 -- From Test module ==========================================================
 
---- Open a top window that displays the list of Apex tests in the current file.
---- Allows to select/deselect tests and execute.
+--- Open a top-split window to display the Apex tests in the current file.
+--- This window also enables to select tests from multiple files.
 Sf.open_test_select = Test.open
 
---- Run the Apex test under the cursor in target_org. The command is sent to SFTerm.
+--- Run the Apex test under the cursor.
+--- It uses Treesitter to determine @IsTest method.
 Sf.run_current_test = Test.run_current_test
 
---- Run all Apex tests in the current Apex file in target_org. The command is sent to SFTerm.
+--- Run all Apex tests in the current Apex file.
 Sf.run_all_tests_in_this_file = Test.run_all_tests_in_this_file
 
---- Repeat the last executed Apex test command. The command is sent to SFTerm.
+--- A convenient way to repeat the last executed Apex test command.
 Sf.repeat_last_tests = Test.repeat_last_tests
 
 return Sf
