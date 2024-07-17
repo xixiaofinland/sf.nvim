29dffb61877dd797a15679150efa5ffd43700c2d
xi xiao <tdxiaoxi2@gmail.com>
2024-07-17 21:33:32 +0300

    ref

--- lua/sf/org.lua
+++ lua/sf/org.lua
@@ -24,11 +24,15 @@ function Org.diff_in_org()
 end
 
 function Org.open()
-  H.open()
+  local cmd = 'sf org open -o ' .. U.get()
+  local err_msg = 'Command failed: ' .. cmd
+  U.silent_job_call(cmd, nil, err_msg)
 end
 
 function Org.open_current_file()
-  H.open_current_file()
+  local cmd = vim.fn.expandcmd('sf org open --source-file "%:p" -o ') .. U.get()
+  local err_msg = 'Command failed: ' .. cmd
+  U.silent_job_call(cmd, nil, err_msg)
 end
 
 -- helpers;
@@ -37,19 +41,6 @@ H.orgs = {}
 
 H.clean_org_cache = function()
   H.orgs = {}
-end
-
-H.open = function()
-  local cmd = 'sf org open -o ' .. U.get()
-  local err_msg = 'Command failed: ' .. cmd
-  U.silent_job_call(cmd, nil, err_msg)
-end
-
-H.open_current_file = function()
-  local cmd = vim.fn.expandcmd('sf org open --source-file "%:p" -o ') .. U.get()
-  print(cmd)
-  local err_msg = 'Command failed: ' .. cmd
-  U.silent_job_call(cmd, nil, err_msg)
 end
 
 H.set_target_org = function()
