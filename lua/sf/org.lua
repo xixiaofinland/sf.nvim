d87959d038fdc0e9b11f0e747926f628e4992099
ognjen-vuceljic <102177460+ognjen-vuceljic@users.noreply.github.com>
2024-07-17 19:22:01 +0200

    fetch current org details from utils.lua

--- lua/sf/org.lua
+++ lua/sf/org.lua
@@ -43,14 +43,14 @@ end
 
 H.open = function()
   local org = string.gsub(choice, '%[S%] ', '')
-  local cmd = 'sf org open'
+  local cmd = 'sf org open -o' .. U.get()
   local err_msg = org .. ' - open command failed!'
   U.silent_job_call(cmd, nil, err_msg, nil)
 end
 
 H.open_current_file = function()
   local org = string.gsub(choice, '%[S%] ', '')
-  local cmd = 'sf org open --source-file "%:p"'
+  local cmd = 'sf org open --source-file "%:p" -o' .. U.get()
   local err_msg = org .. ' - open command failed for the current file!'
   U.silent_job_call(cmd, nil, err_msg, nil)
 end
