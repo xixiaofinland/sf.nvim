831bd56dd577d880b1d82a00138013b262902c6e
xi xiao <tdxiaoxi2@gmail.com>
2024-07-26 21:57:24 +0300

    test: add basic unit tests

--- tests/test_sf.lua
+++ tests/test_sf.lua
@@ -8,20 +8,21 @@ local T = new_set({
     pre_case = function()
       child.setup()
       child.lua([[M = require('sf')]])
+      child.lua([[U = require('sf.util')]])
     end,
     post_once = child.stop,
   },
 })
 
-T['get()'] = MiniTest.new_set()
+T['get_target_org()'] = MiniTest.new_set()
 
-T['get()']['target_org not empty then success'] = function()
-  child.lua([[M.target_org = 'sandbox1']])
-  eq(child.lua([[return M.get()]]), 'sandbox1')
+T['get_target_org()']['it works to get target_org value'] = function()
+  child.lua([[U.target_org = 'sandbox1']])
+  eq(child.lua([[return M.get_target_org()]]), 'sandbox1')
 end
 
-T['get()']['target_org empty then err'] = function()
-  expect.error(function() child.lua([[M.get()]]) end)
+T['get_target_org()']['target_org empty then err'] = function()
+  expect.error(function() child.lua([[M.get_target_org()]]) end)
 end
 
 return T
