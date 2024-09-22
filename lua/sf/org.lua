d90ac39976aebb27a2194a3c8d078d9eb7f51209
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-09-22 19:44:59 -0300

    feat: add download and open log file

--- lua/sf/org.lua
+++ lua/sf/org.lua
@@ -48,10 +48,21 @@ H.pull_log = function()
     if U.is_empty_str(U.target_org) then
         return U.show_err('Target_org empty!')
     end
+    if not U.is_installed('fzf-lua') then
+      return U.show_err('fzf-lua is not installed. Need it to show the list.')
+    end
 
     U.show('Querying logs...')
+    local log_id
+    local on_pull = function(obj)
+        if obj.code ~= 0 then
+            return U.show_err('Failed to download log from org')
+        end
 
-    local on_exit = function (obj)
+        U.try_open_file(U.get_plugin_folder_path() .. "logs/" .. log_id .. ".log")
+    end
+
+    local on_list = function (obj)
         if obj.code ~= 0 then
             return U.show_err('Failed to get logs from org')
         end
@@ -72,24 +83,34 @@ H.pull_log = function()
             logs[name] = v
         end
 
-        require('fzf-lua').fzf_exec(log_names, {fzf_opts = {
-            ['--preview-window'] = 'nohidden,down,50%',
-            ['--preview'] = function(items)
-                local contents = {}
-                local prepend_char = ""
-                vim.tbl_map(
-                    function(x)
-                        table.insert(contents, prepend_char .. U.table_to_string_lines(logs[x])) 
-                        prepend_char = "\n"
+        require('fzf-lua').fzf_exec(log_names, {
+            fzf_opts = {
+                ['--preview-window'] = 'nohidden,down,50%',
+                ['--preview'] = function(items)
+                    local contents = {}
+                    local prepend_char = ""
+                    vim.tbl_map(
+                            function(x)
+                            table.insert(contents, prepend_char .. U.table_to_string_lines(logs[x]))
+                            prepend_char = "\n"
+                        end
+                    , items)
+                    return contents
                     end
-                , items)
-                return contents
+            },
+            actions = {
+                ['default'] = function(selected)
+                    log_id = logs[selected[1]]["Id"]
+                    U.show('Downloading log...')
+                    local get_cmd = B:new():cmd('apex'):act('get'):subact('log'):addParams("-i", log_id):addParams("-d", U.get_plugin_folder_path() .. "logs/"):buildAsTable()
+                    vim.system(get_cmd, {}, vim.schedule_wrap(on_pull))
                 end
-        }})
+            }
+            })
     end
 
     local cmd_tbl = B:new():cmd('apex'):act('list'):subact('log'):addParams('--json'):buildAsTable()
-    vim.system(cmd_tbl, {}, vim.schedule_wrap(on_exit))
+    vim.system(cmd_tbl, {}, vim.schedule_wrap(on_list))
 end
 
 H.orgs = {}
