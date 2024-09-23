435578cd5797d29bde77c5aa927d749433f38d09
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-09-23 11:15:02 -0300

    chore: format ctags.lua

--- lua/sf/ctags.lua
+++ lua/sf/ctags.lua
@@ -1,17 +1,18 @@
-local U = require('sf.util')
+local U = require("sf.util")
 local M = {}
 
 M.create = function()
-  U.is_ctags_installed();
-  local cmd = string.format('ctags --extras=+q --langmap=java:.cls.trigger -f ./tags -R **%sclasses/**', vim.g.sf.default_dir)
+  U.is_ctags_installed()
+  local cmd =
+    string.format("ctags --extras=+q --langmap=java:.cls.trigger -f ./tags -R **%sclasses/**", vim.g.sf.default_dir)
   U.silent_job_call(cmd, "Tags updated successfully.", "Error updating tags.")
 end
 
 M.create_and_list = function()
-  if not U.is_installed('fzf-lua') then
-    return U.show_err('fzf-lua is not installed. Need it to show the list.')
+  if not U.is_installed("fzf-lua") then
+    return U.show_err("fzf-lua is not installed. Need it to show the list.")
   end
   M.create()
-  require('fzf-lua').tags()
+  require("fzf-lua").tags()
 end
 return M
